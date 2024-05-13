809db78b3bd4d57f708183789a139fd1597a34e4
nimofarah <faranim01@gmail.com>
2024-05-14 02:31:21 +0300

    x

---
+++ main.js
@@ -0,0 +1,7 @@
+const map = L.map('map').setView([60.1695, 24.9354], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
